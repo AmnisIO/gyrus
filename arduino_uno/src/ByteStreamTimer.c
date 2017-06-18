a2b5ed55e6bb455d42752f6edbd96adea182b0d7
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-18 19:55:45 +0530

    Add ByteStreamTimer implementation

---
+++ arduino_uno/src/ByteStreamTimer.c
@@ -0,0 +1,13 @@
+#include "ByteStreamTimer.h"
+
+ByteStreamTimerTaskIdentifier byte_stream_timer_set_timeout(byte_stream_timer_task task, void *argument, int delay_in_milliseconds) {
+
+}
+
+void byte_stream_timer_clear_timeout(ByteStreamTimerTaskIdentifier task_id) {
+
+}
+
+ByteStreamTimerTaskIdentifier byte_stream_timer_set_interval(byte_stream_timer_task task, void *argument, int period_in_milliseconds) {
+
+}
