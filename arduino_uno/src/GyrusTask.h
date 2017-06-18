144bfb6afc0f93f9a1885907bf70d3a780c85b98
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-18 19:54:52 +0530

    Add GyrusTask

---
+++ arduino_uno/src/GyrusTask.h
@@ -0,0 +1,25 @@
+#ifndef GYRUS_ARDUINO_UNO_GYRUSTASKARDUINO_H
+#define GYRUS_ARDUINO_UNO_GYRUSTASKARDUINO_H
+
+typedef enum GyrusTaskType {
+  GYRUS_TASK_TYPE_NONE;
+  GYRUS_TASK_TYPE_SINGLE;
+  GYRUS_TASK_TYPE_PERIODIC;
+} GyrusTaskType;
+
+typedef unsigned long Milliseconds;
+typedef int TaskIdentifier;
+typedef void *(*gyrus_task_callback)(void *argument);
+
+typedef struct GyrusTask {
+  TaskIdentifier id;
+  GyrusTaskType type;
+  gyrus_task_callback callback;
+  void *argument;
+  Milliseconds last_called_at;
+  int period;
+} GyrusTask;
+
+GyrusTask *gyrus_task_create(gyrus_task_callback callback, void* argument, GyrusTaskType type, int period);
+
+#endif //GYRUS_ARDUINO_UNO_GYRUSTASKARDUINO_H
