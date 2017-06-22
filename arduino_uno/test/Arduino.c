5a785db4e7524fc70b133eb5b398d1ff9dc06a0f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-23 01:44:52 +0530

    Add digitalWrite function

--- arduino_uno/test/Arduino.c
+++ arduino_uno/test/Arduino.c
@@ -1,11 +1,16 @@
+#include <printf.h>
 #include "Arduino.h"
 
 unsigned long milliseconds = 0UL;
 
-unsigned long millis() {
+unsigned long millis () {
   return milliseconds++;
 }
 
-int digitalRead(Byte pin) {
+int digitalRead (Byte pin) {
   return pin;
+}
+
+void digitalWrite (Byte pin, Byte value) {
+  printf ("wrote %d to pin %d\n", pin, value);
 }
