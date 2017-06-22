9501baadb110043e570c9fec581f9979bffc0333
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-23 01:45:07 +0530

    Add user-based test for sources, sinks, producer and listener

--- arduino_uno/test/main.c
+++ arduino_uno/test/main.c
@@ -1,24 +1,17 @@
 #include <Arduino.h>
-#include <unistd.h>
 #include <RivuletTimer.h>
-#include "ByteStream.h"
-#include "DigitalReadProducer.h"
-
-void _next (ByteListener *self, Byte v) {
-  printf ("%d\n", v);
-}
-void _error (ByteListener *self, int error) {
-  printf ("%d\n", error);
-}
-void _complete (ByteListener *self) {
-  printf ("completed\n");
-}
+#include <Sources.h>
+#include <Sinks.h>
+#include <DigitalWriteListener.h>
+#include <unistd.h>
 
 int main () {
   rivulet_timer_initialize (millis);
-  ByteStream *pin10$ = digital_read_stream_create (10);
-  ByteListener *listener = byte_listener_create (_next, _error, _complete);
-  pin10$->add_listener (pin10$, listener);
+  Sources *sources = sources_create ();
+  Sinks *sinks = sinks_create ();
+  sinks->LED$ = sources->D10$->map_to (sources->D10$, 15);
+  void *listener = digital_write_listener_create (13);
+  sinks->LED$->add_listener (sinks->LED$, (ByteListener *) listener);
   for (int i = 0; i < 10; i++) {
     rivulet_timer->tick ();
     sleep (1);
