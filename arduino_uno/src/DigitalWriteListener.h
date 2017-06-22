ea9e556f22a272e4259a90af800467be5992b9a6
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-23 01:44:30 +0530

    Add DigitalWriteListener

--- arduino_uno/src/DigitalWriteListener.h
+++ arduino_uno/src/DigitalWriteListener.h
@@ -3,6 +3,17 @@
 #ifndef GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
 #define GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
 
-ByteListener *digital_write_listener_create(Byte pin);
+typedef struct DigitalWriteListener {
+  ByteObservableType type;
+  byte_listener_internal_next _next;
+  byte_listener_internal_error _error;
+  byte_listener_internal_complete _complete;
+  byte_listener_next next;
+  byte_listener_error error;
+  byte_listener_complete complete;
+  Byte pin;
+} DigitalWriteListener;
+
+DigitalWriteListener *digital_write_listener_create(Byte pin);
 
 #endif //GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
