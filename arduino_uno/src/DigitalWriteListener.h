6e6b94d318b3b22c55d92494d6ebec81322beeda
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-23 01:26:41 +0530

    Add digital write listener

---
+++ arduino_uno/src/DigitalWriteListener.h
@@ -0,0 +1,8 @@
+#include <ByteListener.h>
+
+#ifndef GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
+#define GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
+
+ByteListener *digital_write_listener_create(Byte pin);
+
+#endif //GYRUS_ARDUINO_UNO_DIGITALWRITELISTENER_H
