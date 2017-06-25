54c233998bbbfdbba7ad239923815d48d1bde764
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-26 02:07:19 +0530

    Write only LOW/HIGH values

--- arduino_uno/src/DigitalWriteListener.c
+++ arduino_uno/src/DigitalWriteListener.c
@@ -4,11 +4,12 @@
 
 static void _next (ByteListener *self, Byte value) {
   DigitalWriteListener *listener = (DigitalWriteListener *) self;
-  if (listener->_started) return digitalWrite (listener->pin, value);
+  Byte output = value == LOW ? LOW : HIGH;
+  if (listener->_started) return digitalWrite (listener->pin, output);
   digitalWrite (listener->pin, LOW);
   pinMode (listener->pin, OUTPUT);
   listener->_started = true;
-  digitalWrite (listener->pin, value);
+  digitalWrite (listener->pin, output);
 }
 
 static void _error (ByteListener *self, int e) {
