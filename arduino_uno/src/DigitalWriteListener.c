b3b0ab10e22252cbc30105982d387ceb630127d3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-26 01:47:25 +0530

    Simplify names

--- arduino_uno/src/DigitalWriteListener.c
+++ arduino_uno/src/DigitalWriteListener.c
@@ -2,27 +2,26 @@
 #include "DigitalWriteListener.h"
 #include "Arduino.h"
 
-static void _digital_write_next (ByteListener *self, Byte value) {
+static void _next (ByteListener *self, Byte value) {
   DigitalWriteListener *listener = (DigitalWriteListener *) self;
-  if(!listener->_started) {
+  if (!listener->_started) {
     pinMode (listener->pin, OUTPUT);
     listener->_started = true;
   }
   digitalWrite (listener->pin, value);
 }
 
-static void _digital_write_error (ByteListener *self, int e) {
+static void _error (ByteListener *self, int e) {
   // ignore
 }
 
-static void _digital_write_complete (ByteListener *self) {
+static void _complete (ByteListener *self) {
   // ignore
 }
 
 ByteListener *digital_write_listener_create (Byte pin) {
   DigitalWriteListener *listener = xmalloc (sizeof (DigitalWriteListener));
-  byte_listener_initialize ((ByteListener *) listener, _digital_write_next, _digital_write_error,
-                            _digital_write_complete);
+  byte_listener_initialize ((ByteListener *) listener, _next, _error, _complete);
   listener->pin = pin;
   listener->_started = false;
   return (ByteListener *) listener;
