9ecfd8ba78599e3a358b77193db243b8019f918a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-07 22:57:13 +0530

    Add AnalogWriteListener

---
+++ arduino_uno/src/AnalogWriteListener.h
@@ -0,0 +1,22 @@
+#ifndef GYRUS_ARDUINO_UNO_ANALOGWRITELISTENER_H
+#define GYRUS_ARDUINO_UNO_ANALOGWRITELISTENER_H
+
+#include <RivuletListener.h>
+#include <Boolean.h>
+#include <Arduino.h>
+
+typedef struct AnalogWriteListener {
+  RivuletObservableType type;
+  rivulet_listener_internal_next _next;
+  rivulet_listener_internal_error _error;
+  rivulet_listener_internal_complete _complete;
+  rivulet_listener_next next;
+  rivulet_listener_error error;
+  rivulet_listener_complete complete;
+  int pin;
+  Boolean _started;
+} AnalogWriteListener;
+
+RivuletListener *analog_write_listener_create(int pin);
+
+#endif //GYRUS_ARDUINO_UNO_ANALOGWRITELISTENER_H
