c8025d784e2f2d6e44115a69bd0bcfa920e2132f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-07 22:52:13 +0530

    Add Analog Read Producer implementation

---
+++ arduino_uno/src/AnalogReadProducer.c
@@ -0,0 +1,34 @@
+#include <RivuletStream.h>
+#include "RivuletTimer.h"
+#include <Arduino.h>
+#include "AnalogReadProducer.h"
+
+static void _pin_read (void *self) {
+  AnalogReadProducer *producer = self;
+  rivulet_listener_internal_next next = rivulet_listener_internal_next_get (producer->_listener);
+  int value = analogRead (producer->_pin);
+  next (producer->_listener, value);
+}
+
+static void _pin_read_start (RivuletProducer *self, RivuletListenerInternal *listener) {
+  AnalogReadProducer *producer = (AnalogReadProducer *) self;
+  producer->_listener = listener;
+  producer->_task_id = rivulet_timer->set_interval (_pin_read, producer, 1);
+  pinMode (producer->_pin, INPUT);
+}
+
+static void _pin_read_stop (RivuletProducer *self) {
+  AnalogReadProducer *producer = (AnalogReadProducer *) self;
+  rivulet_timer->clear_task (producer->_task_id);
+}
+
+AnalogReadProducer *analog_read_producer_create (int pin) {
+  AnalogReadProducer *producer = xmalloc (sizeof (AnalogReadProducer));
+  rivulet_producer_initialize ((RivuletProducer *) producer, _pin_read_start, _pin_read_stop);
+  producer->_pin = pin;
+  return producer;
+}
+
+RivuletStream *analog_read_stream_create (int pin) {
+  return rivulet_stream_create ((RivuletProducer *) analog_read_producer_create (pin));
+}
