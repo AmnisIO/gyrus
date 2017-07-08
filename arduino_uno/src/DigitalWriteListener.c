d69e9cc92d3292a79975b6037ffe1d28722aef22
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 16:35:02 +0530

    Use Pin Mode, Boolean, and Signal Level from GyrusUtils

--- arduino_uno/src/DigitalWriteListener.c
+++ arduino_uno/src/DigitalWriteListener.c
@@ -1,15 +1,14 @@
 #include <RivuletListernerManager.h>
 #include <Arduino.h>
 #include "DigitalWriteListener.h"
-#include "SignalLevels.h"
 
 static void _next (RivuletListener *self, int value) {
   DigitalWriteListener *listener = (DigitalWriteListener *) self;
-  int output = value == GYRUS_LOW ? LOW : HIGH;
+  SignalLevel output = value == GYRUS_SIGNAL_LEVEL_LOW ? LOW : HIGH;
   if (listener->_started) return digitalWrite (listener->pin, output);
   digitalWrite (listener->pin, LOW);
-  pinMode (listener->pin, OUTPUT);
-  listener->_started = true;
+  pinMode (listener->pin, GYRUS_PIN_MODE_OUTPUT);
+  listener->_started = GYRUS_TRUE;
   digitalWrite (listener->pin, output);
 }
 
@@ -25,6 +24,6 @@ RivuletListener *digital_write_listener_create (Pin pin) {
   DigitalWriteListener *listener = xmalloc (sizeof (DigitalWriteListener));
   rivulet_listener_initialize ((RivuletListener *) listener, _next, _error, _complete);
   listener->pin = pin;
-  listener->_started = false;
+  listener->_started = GYRUS_FALSE;
   return (RivuletListener *) listener;
 }
