4f37c7e7f8ed7f2dfb5934f739c4bfdcf2eefa16
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 16:33:49 +0530

    Use Gyrus Utils

--- arduino_uno/lib/Arduino.h
+++ arduino_uno/lib/Arduino.h
@@ -2,26 +2,16 @@
 #define GYRUS_ARDUINO_UNO_ARDUINO_H
 
 #include "Boolean.h"
-#include "Pin.h"
+#include "GyrusUtils.h"
 
 unsigned long millis();
-int digitalRead(Pin pin);
-int analogRead(Pin pin);
-void digitalWrite(Pin pin, int value);
-void analogWrite(Pin pin, int value);
-void pinMode(Pin pin, Boolean mode);
-extern Boolean INPUT;
-extern Boolean OUTPUT;
-extern Boolean true;
-extern Boolean false;
-extern Boolean HIGH;
-extern Boolean LOW;
+void pinMode(Pin, PinMode);
+void digitalWrite(Pin, SignalLevel);
+int digitalRead(Pin);
+int analogRead(Pin);
+void analogWrite(Pin, int);
 
-extern Pin A0;
-extern Pin A1;
-extern Pin A2;
-extern Pin A3;
-extern Pin A4;
-extern Pin A5;
+SignalLevel LOW;
+SignalLevel HIGH;
 
 #endif //GYRUS_ARDUINO_UNO_ARDUINO_H
