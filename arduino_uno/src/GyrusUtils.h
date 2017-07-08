cf8d312ab54454bb9f8b241ee199d83477f78a9d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 00:27:43 +0530

    Declare const as extern for arduino

--- arduino_uno/src/GyrusUtils.h
+++ arduino_uno/src/GyrusUtils.h
@@ -4,40 +4,40 @@
 #include <stdint.h>
 
 typedef uint8_t Pin;
-const Pin GYRUS_PIN_D0;
-const Pin GYRUS_PIN_D1;
-const Pin GYRUS_PIN_D2;
-const Pin GYRUS_PIN_D3;
-const Pin GYRUS_PIN_D4;
-const Pin GYRUS_PIN_D5;
-const Pin GYRUS_PIN_D6;
-const Pin GYRUS_PIN_D7;
-const Pin GYRUS_PIN_D8;
-const Pin GYRUS_PIN_D9;
-const Pin GYRUS_PIN_D10;
-const Pin GYRUS_PIN_D11;
-const Pin GYRUS_PIN_D12;
-const Pin GYRUS_PIN_D13;
-const Pin GYRUS_PIN_LED;
-const Pin GYRUS_PIN_A0;
-const Pin GYRUS_PIN_A1;
-const Pin GYRUS_PIN_A2;
-const Pin GYRUS_PIN_A3;
-const Pin GYRUS_PIN_A4;
-const Pin GYRUS_PIN_A5;
-const Pin GYRUS_PIN_A6;
-const Pin GYRUS_PIN_A7;
+extern const Pin GYRUS_PIN_D0;
+extern const Pin GYRUS_PIN_D1;
+extern const Pin GYRUS_PIN_D2;
+extern const Pin GYRUS_PIN_D3;
+extern const Pin GYRUS_PIN_D4;
+extern const Pin GYRUS_PIN_D5;
+extern const Pin GYRUS_PIN_D6;
+extern const Pin GYRUS_PIN_D7;
+extern const Pin GYRUS_PIN_D8;
+extern const Pin GYRUS_PIN_D9;
+extern const Pin GYRUS_PIN_D10;
+extern const Pin GYRUS_PIN_D11;
+extern const Pin GYRUS_PIN_D12;
+extern const Pin GYRUS_PIN_D13;
+extern const Pin GYRUS_PIN_LED;
+extern const Pin GYRUS_PIN_A0;
+extern const Pin GYRUS_PIN_A1;
+extern const Pin GYRUS_PIN_A2;
+extern const Pin GYRUS_PIN_A3;
+extern const Pin GYRUS_PIN_A4;
+extern const Pin GYRUS_PIN_A5;
+extern const Pin GYRUS_PIN_A6;
+extern const Pin GYRUS_PIN_A7;
 
 typedef uint8_t PinMode;
-const PinMode GYRUS_PIN_MODE_INPUT;
-const PinMode GYRUS_PIN_MODE_OUTPUT;
+extern const PinMode GYRUS_PIN_MODE_INPUT;
+extern const PinMode GYRUS_PIN_MODE_OUTPUT;
 
 typedef uint8_t SignalLevel;
-const SignalLevel GYRUS_SIGNAL_LEVEL_LOW;
-const SignalLevel GYRUS_SIGNAL_LEVEL_HIGH;
+extern const SignalLevel GYRUS_SIGNAL_LEVEL_LOW;
+extern const SignalLevel GYRUS_SIGNAL_LEVEL_HIGH;
 
 typedef uint8_t Boolean;
-const Boolean GYRUS_TRUE;
-const Boolean GYRUS_FALSE;
+extern const Boolean GYRUS_TRUE;
+extern const Boolean GYRUS_FALSE;
 
 #endif //GYRUS_ARDUINO_UNO_GYRUSUTILS_H
