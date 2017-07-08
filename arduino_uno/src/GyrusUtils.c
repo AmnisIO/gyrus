3a24c4d9c4ccc5f5b3548941d8daa4c7781db146
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-08 16:33:10 +0530

    Implement Gyrus Utils for Arduino UNO

---
+++ arduino_uno/src/GyrusUtils.c
@@ -0,0 +1,34 @@
+#include "GyrusUtils.h"
+
+const Pin GYRUS_PIN_D0 = 0;
+const Pin GYRUS_PIN_D1 = 1;
+const Pin GYRUS_PIN_D2 = 2;
+const Pin GYRUS_PIN_D3 = 3;
+const Pin GYRUS_PIN_D4 = 4;
+const Pin GYRUS_PIN_D5 = 5;
+const Pin GYRUS_PIN_D6 = 6;
+const Pin GYRUS_PIN_D7 = 7;
+const Pin GYRUS_PIN_D8 = 8;
+const Pin GYRUS_PIN_D9 = 9;
+const Pin GYRUS_PIN_D10 = 10;
+const Pin GYRUS_PIN_D11 = 11;
+const Pin GYRUS_PIN_D12 = 12;
+const Pin GYRUS_PIN_D13 = 13;
+const Pin GYRUS_PIN_LED = 13;
+const Pin GYRUS_PIN_A0 = 14;
+const Pin GYRUS_PIN_A1 = 15;
+const Pin GYRUS_PIN_A2 = 16;
+const Pin GYRUS_PIN_A3 = 17;
+const Pin GYRUS_PIN_A4 = 18;
+const Pin GYRUS_PIN_A5 = 19;
+const Pin GYRUS_PIN_A6 = 20;
+const Pin GYRUS_PIN_A7 = 21;
+
+const PinMode GYRUS_PIN_MODE_INPUT = 0;
+const PinMode GYRUS_PIN_MODE_OUTPUT = 1;
+
+const SignalLevel GYRUS_SIGNAL_LEVEL_LOW = 0;
+const SignalLevel GYRUS_SIGNAL_LEVEL_HIGH = 255;
+
+const Boolean GYRUS_TRUE = 1;
+const Boolean GYRUS_FALSE = 0;
