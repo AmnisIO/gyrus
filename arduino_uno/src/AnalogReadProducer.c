d2787bffd59c2ff52a2d10f16c49c87e8fa54e1a
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 14:30:38 +0530

    Register producers

--- arduino_uno/src/AnalogReadProducer.c
+++ arduino_uno/src/AnalogReadProducer.c
@@ -1,30 +1,44 @@
 #include <RivuletStream.h>
 #include "RivuletTimer.h"
 #include <Arduino.h>
+#include <RivuletProducerRegistry.h>
+#include <RivuletListenerRegistry.h>
 #include "AnalogReadProducer.h"
 
 static void _pin_read (void *self) {
   AnalogReadProducer *producer = self;
-  rivulet_listener_internal_next next = rivulet_listener_internal_next_get (producer->_listener);
   int value = analogRead (producer->_pin);
-  next (producer->_listener, value);
+  RivuletListener *listener = producer->_listener;
+  rivulet_listener_registry_get_next (listener) (listener, value);
 }
 
-static void _pin_read_start (RivuletProducer *self, RivuletListenerInternal *listener) {
+static void _start (RivuletProducer *self, RivuletListener *listener) {
   AnalogReadProducer *producer = (AnalogReadProducer *) self;
   producer->_listener = listener;
   producer->_task_id = rivulet_timer->set_interval (_pin_read, producer, 1);
   pinMode (producer->_pin, GYRUS_PIN_MODE_INPUT);
 }
 
-static void _pin_read_stop (RivuletProducer *self) {
+static void _stop (RivuletProducer *self) {
   AnalogReadProducer *producer = (AnalogReadProducer *) self;
   rivulet_timer->clear_task (producer->_task_id);
 }
 
+static Boolean _registered = 0;
+static RivuletListenerType _listener_type = 0;
+static RivuletProducerType _producer_type = 0;
+
+static void _register () {
+  if (_registered) return;
+  _producer_type = rivulet_producer_registry_register (_start, _stop);
+  _registered = 1;
+}
+
 AnalogReadProducer *analog_read_producer_create (Pin pin) {
   AnalogReadProducer *producer = xmalloc (sizeof (AnalogReadProducer));
-  rivulet_producer_initialize ((RivuletProducer *) producer, _pin_read_start, _pin_read_stop);
+  _register ();
+  producer->listener_type = _listener_type;
+  producer->producer_type = _producer_type;
   producer->_pin = pin;
   return producer;
 }
