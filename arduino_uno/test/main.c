c2add6e715f51b01effbba8bf1da95c7ef14df2f
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-07-09 14:46:34 +0530

    Fix tests

--- arduino_uno/test/main.c
+++ arduino_uno/test/main.c
@@ -6,23 +6,23 @@ int brightness = 0;
 
 int change = 5;
 
-int getCurrentBrightness(int event) {
+int getCurrentBrightness (int event) {
   brightness = brightness + change;
   if (brightness <= GYRUS_SIGNAL_LEVEL_LOW || brightness >= GYRUS_SIGNAL_LEVEL_HIGH) change = -change;
   return brightness;
 }
 
-int toggle(int value) {
+int toggle (int value) {
   return value == GYRUS_SIGNAL_LEVEL_LOW ? GYRUS_SIGNAL_LEVEL_HIGH : GYRUS_SIGNAL_LEVEL_LOW;
 }
 
-Sinks* application(Sources* arduino) {
-  Sinks* sinks = sinks_create();
-  RivuletStream* _typewriter_intermediary_1 = rivulet_stream_periodic(3);
-  RivuletStream* _typewriter_intermediary_2 = rivulet_stream_periodic(1);
-  sinks->D10 = _typewriter_intermediary_1->map(_typewriter_intermediary_1, getCurrentBrightness);
-  RivuletStream* _sampled = _typewriter_intermediary_2->sample(_typewriter_intermediary_2, arduino->LED);
-  sinks->LED = _sampled->map(_sampled, toggle);
+Sinks *application (Sources *arduino) {
+  Sinks *sinks = sinks_create ();
+  RivuletStream *_typewriter_intermediary_1 = rivulet_stream_periodic (3);
+  RivuletStream *_typewriter_intermediary_2 = rivulet_stream_periodic (1);
+  sinks->D10 = rivulet_stream_map (_typewriter_intermediary_1, getCurrentBrightness);
+  RivuletStream *_sampled = rivulet_stream_sample (_typewriter_intermediary_2, arduino->LED);
+  sinks->LED = rivulet_stream_map (_sampled, toggle);
   return sinks;
 }
 
